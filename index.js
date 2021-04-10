38a0d132ffd82927ed720add0be35122e19087bd
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-04-10 20:42:27 +0200

    displayPage function

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-const objectImg = document.getElementById('object-img');
-const objectTitle = document.getElementById('object-title');
-const objectPrice = document.getElementById('object-price');
-const articles = document.getElementById('articles');
+const articles = document.getElementById('articles'); 
 const templateElt = document.getElementById('template');
-
 const cloneElt = document.importNode(templateElt.content, true); 
 
 main(); 
 
 async function main() {
     const products = await getProducts();
-    
-    displayPages(products); 
+    console.log(products);
 
+    products.forEach(product => {
+        displayPages(product); 
+    });
 }
 
 async function getProducts() {
     return fetch('http://localhost:3000/api/cameras')
-        .then((httpBodyResponse) => httpBodyResponse.json())
-        .then((products) => products)
-        .catch((error) => {
-            alert(
-                "la connexion au serveur n'a pas pu être effectué"
+    .then((httpBodyResponse) => httpBodyResponse.json())
+    .catch((error) => {
+        alert(
+            "la connexion au serveur n'a pas pu être effectué"
             )
         })
-}; 
-
-function displayPages(products) {
-    // Clonage du template 
-    console.log(products)
+    }; 
     
-    cloneElt.objectImg.src = products.imageUrl;
-    cloneElt.objectTitle.textContent = products.name;
-    cloneElt.objectPrice.textContent = products.price;
-    
-    articles.appendChild(cloneElt); 
-
-}
+    function displayPages(product) {
+        // Clonage du template 
+        console.log(product);
+        console.log(product.imageUrl);
+        console.log(product.name);
+        console.log(product.price);
+        console.log(product._id);
+        
+        
+        cloneElt.getElementById('object-img').src = product.imageUrl;
+        cloneElt.getElementById('object-title').textContent = product.name;
+        cloneElt.getElementById('object-price').textContent = `${product.price / 100}.00 €`;
+        cloneElt.getElementById('object-link').href = `/products.html?id=${product._id}`;
+        
+        console.log(cloneElt); 
+        
+        var newObject = document.getElementById('articles').appendChild(cloneElt); 
+    }
