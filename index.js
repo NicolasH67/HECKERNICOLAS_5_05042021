01232427a504f86310962a068164f7666decb9a7
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-04-11 13:48:41 +0200

    fin displayPages function + add icons bootstrap

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const articles = document.getElementById('articles'); 
 const templateElt = document.getElementById('template');
-const cloneElt = document.importNode(templateElt.content, true); 
 
 main(); 
 
 async function main() {
     const products = await getProducts();
     console.log(products);
-
+    
     products.forEach(product => {
         displayPages(product); 
     });
@@ -23,21 +21,25 @@ async function getProducts() {
         })
     }; 
     
-    function displayPages(product) {
-        // Clonage du template 
-        console.log(product);
-        console.log(product.imageUrl);
-        console.log(product.name);
-        console.log(product.price);
-        console.log(product._id);
+function displayPages(product) {
+    // Clonage du template 
+    const articles = document.querySelector('#articles'); 
+    const cloneElt = document.importNode(templateElt.content, true); 
         
+    console.log(product);
+    console.log(product.imageUrl);
+    console.log(product.name);
+    console.log(product.price);
+    console.log(product._id);
+
+    cloneElt.getElementById('object-img').src = product.imageUrl;
+    cloneElt.getElementById('object-img').alt = 'image du '+product.name;
+    cloneElt.getElementById('object-title').textContent = product.name;
+    cloneElt.getElementById('object-price').textContent = `${product.price / 100}.00 €`;
+    cloneElt.getElementById('object-link').href = `/products.html?id=${product._id}`;
         
-        cloneElt.getElementById('object-img').src = product.imageUrl;
-        cloneElt.getElementById('object-title').textContent = product.name;
-        cloneElt.getElementById('object-price').textContent = `${product.price / 100}.00 €`;
-        cloneElt.getElementById('object-link').href = `/products.html?id=${product._id}`;
-        
-        console.log(cloneElt); 
-        
-        var newObject = document.getElementById('articles').appendChild(cloneElt); 
-    }
+    console.log(cloneElt); 
+
+    var newObject = articles.appendChild(cloneElt)
+
+}
