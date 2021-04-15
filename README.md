aaa50d48d29bcbe7e5ac5fdd9eb8c4fd75e6221c
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-04-15 15:38:11 +0200

    add localStorage function

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Orinoco #
 
-This is the back end server for Project 5 of the Junior Web Developer path.
+
+English 
 
 ### Prerequisites ###
 
@@ -8,8 +9,18 @@ You will need to have Node and `npm` installed locally on your machine.
 
 ### Installation ###
 
-Clone this repo. From within the project folder, run `npm install`. You 
-can then run the server with `node server`. 
-The server should run on `localhost` with default port `3000`. If the
-server runs on another port for any reason, this is printed to the
-console when the server starts, e.g. `Listening on port 3001`.
+Clone this repo. 
+From within the project folder, run `npm install`. 
+You can then run the server with `node server`. 
+
+Francais 
+
+### Condition préalable ###
+
+Installés localement Node et 'npm' sur votre machine 
+
+### Installation ###
+
+Clonez ce dépôt. 
+Depuis le dossier du projet, exécutez `npm install`.
+Exécuter la commande `node server`. 
--- basket.js
+++ basket.js
@@ -10,7 +10,27 @@ basket.addEventListener('click', (e) =>  {
 // Recovery info 
 
 btnAdd.addEventListener('click', function e() {
-    let id = window.location.search.substr(1);
-    let objectif = select.value; 
-    let selectQuantity = quantity.value; 
+    const id = window.location.search.substr(1);
+    const objectif = select.value; 
+    const selectQuantity = quantity.value; 
+    let optionProduit= [id, objectif, selectQuantity]
+
+    //----- localStorage add element ------------
+    let produitLocalStorage = JSON.parse(localStorage.getItem("produit")); 
+    
+    console.log(produitLocalStorage); 
+    
+    if (produitLocalStorage) {
+        produitLocalStorage.push(optionProduit); 
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage)); 
+        console.log(produitLocalStorage); 
+        
+    } 
+    else {
+        produitLocalStorage = []; 
+        produitLocalStorage.push(optionProduit); 
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage)); 
+        console.log(produitLocalStorage); 
+    }
+    
 })
