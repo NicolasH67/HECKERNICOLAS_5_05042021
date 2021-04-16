d2fab08b6c2736f47970083f9007fb3fdd7b48c4
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-04-16 12:37:15 +0200

    basket add name product

--- basket.js
+++ basket.js
@@ -35,10 +35,52 @@ const sectionBasket = document.getElementById('div-basket');
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit")); 
 
 
-console.log(produitLocalStorage)
 if (produitLocalStorage === null) {
     sectionBasket.innerHTML = '<div class="empty-basket"><p class="h2 text-danger font-italic">Votre Panier est vide </p></div>'
 } 
 else {
-    console.log(localStorage)
+    
+    for (let i = 0; i < produitLocalStorage.length; i++) {
+        const product = produitLocalStorage[i];
+        console.log(product);
+        const productId = product[0];
+        console.log(productId);
+        const productObjectif = product[1];
+        const productQuantity = product[2];
+        
+        main()
+
+        async function main() {
+            const completeProduct = await getProduct()
+            console.log(completeProduct)
+            displayPages(completeProduct)
+        }
+
+        async function getProduct() {
+            return fetch(`http://localhost:3000/api/cameras/${productId}`)
+            .then((httpBodyResponse) => httpBodyResponse.json())
+            .catch((error) => {
+                alert(
+                    "la connexion au serveur n'a pas pu être effectué"
+                )
+            }); 
+        }
+
+        
+        function displayPages(product) {
+            const templateElt = document.getElementById('template-basket')
+            // clone template 
+            const cloneElt = document.importNode(templateElt.content, true); 
+    
+            // displayPage
+            cloneElt.getElementById('name').textContent = product.name;
+    
+            // display in page 
+    
+            const basket = document.getElementById('div-basket'); 
+            let newObject = basket.appendChild(cloneElt)
+
+       }
+    }
+
 }
