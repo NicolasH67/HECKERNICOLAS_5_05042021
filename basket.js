b1c7a16816018bd28feb165e691eed770b4c1e62
NicolasH67 <hecker.nicolas.pro@gmail.com>
2021-04-19 17:39:45 +0200

    modif basket

--- basket.js
+++ basket.js
@@ -12,6 +12,7 @@ basket.addEventListener('click', (e) =>  {
 const btnHome = document.getElementById('btnHome'); 
 const btnClear = document.getElementById('btnClear'); 
 const btnCommande = document.getElementById('btnCommande'); 
+const price = []
 
 btnHome.addEventListener('click', (e) =>  {
     window.location.href="./index.html"; 
@@ -26,10 +27,9 @@ btnClear.addEventListener('click', (e) =>  {
 })
 
 btnCommande.addEventListener('click', (e) =>  {
-    window.location.href="./orderConfirm.html"; 
     e.preventDefault()
-    localStorage.clear()
-    
+    window.location.href="./orderConfirm.html"; 
+
 })
 
 //----------------display basket ---------------
@@ -41,7 +41,6 @@ if (produitLocalStorage === null) {
     sectionBasket.innerHTML = '<div class="empty-basket"><p class="h2 text-danger font-italic">Votre Panier est vide </p></div>'
 } 
 else {
-    
     for (let i = 0; i < produitLocalStorage.length; i++) {
         const product = produitLocalStorage[i];
         console.log(product);
@@ -50,8 +49,6 @@ else {
         const productObjectif = product[1];
         const productQuantity = product[2];
         
-
-
         main()
         
         async function main() {
@@ -59,16 +56,45 @@ else {
             console.log(completeProduct)
             displayPages(completeProduct)
             
+            const quantityBasket = document.getElementById(`quantity-basket${completeProduct._id}`); 
+            quantityBasket.value = productQuantity;
+            
+            const sectionPrice = document.getElementById(`price${completeProduct._id}`); 
+            
+            const sectionPriceQuantity = document.getElementById(`quantity-basket${completeProduct._id}`)
+            
+            const productPrice = completeProduct.price
+        
+            const finalPrice = sectionPriceQuantity.value * productPrice
+            
+            sectionPrice.textContent = `Prix : ${finalPrice / 100},00€`
+            
+            console.log(`Prix : ${finalPrice / 100},00€`)
+            
+            price.push(finalPrice)
+
+            console.log(price)
+
+            const reducer = (accumulator, currentValue) => accumulator + currentValue; 
+
+            const totalPrice = price.reduce(reducer); 
+
+            document.getElementById('totalPrice').textContent = `${totalPrice / 100},00€`
+            
             completeProduct.lenses.forEach(objectif => {
                 let option = document.createElement("option"); 
                 option.value = objectif; 
                 option.textContent = objectif;
-    
                 const selectBasket = document.getElementById(`select-basket${completeProduct._id}`)
-                let newOption = selectBasket.appendChild(option); 
+                
+                let newOption = selectBasket.appendChild(option);
+                console.log(productObjectif);
+                
+                // select value 
+                selectBasket.value = productObjectif; 
+                
             }); 
-
-        }
+        }    
         
         async function getProduct() {
             return fetch(`http://localhost:3000/api/cameras/${productId}`)
@@ -85,13 +111,14 @@ else {
             // clone template 
             const templateElt = document.getElementById('template-basket')
             const cloneElt = document.importNode(templateElt.content, true); 
-            
+                
             // displayPage
+            cloneElt.getElementById('article').id = `article${product._id}`;
             cloneElt.getElementById('name').textContent = product.name;
             cloneElt.getElementById('select-basket').id = `select-basket${product._id}`;
             cloneElt.getElementById('quantity-basket').id = `quantity-basket${product._id}`;
-            cloneElt.getElementById('price').textContent = `${product.price * productQuantity / 100}.00 €`;
-            
+            cloneElt.getElementById('price').id = `price${product._id}`
+            cloneElt.getElementById('delete').id = `delete?${product._id}`
                 
             // display in page 
                 
